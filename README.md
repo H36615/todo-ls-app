b8190f0379cd01982f18c0d6709c228f5b9f4f0b
H36615 <h36615@protonmail.com>
2021-10-03 16:49:56 +0300

    Add start-app script

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## Description
 todo-ls is a simple web app for organizing tasks/todo items - a todo list app.
 
-## Demo
-Currently not running on an instance.
+## Demo / Setup
+The app is currently not running on any instance. However, you can just run `start-app.sh` script to get the whole app running and accessible from the browser. Only docker is required.
 
 ## Source code repositories
 [todo-ls backend](https://github.com/H36615/todo-ls-backend)
---
+++ start-app.sh
@@ -0,0 +1,31 @@
+ENV_FILE=".env"
+ENV_EXAMPLE_FILE=".env.example"
+
+if ! test -f $ENV_FILE; then
+	if test -f $ENV_EXAMPLE_FILE; then
+		read -r -p "$ENV_FILE not found. Do you wanna create one by copying '$ENV_EXAMPLE_FILE'? [y/N]" response
+
+		response=${response,,}
+		if [[ "$response" =~ ^(yes|y)$ ]]; then
+			cp $ENV_EXAMPLE_FILE $ENV_FILE
+		else
+			echo "Error: .env not found. (You should create it.)"
+			exit 1
+		fi
+	else
+		echo "Error: .env.example not found."
+		exit 1
+	fi
+fi
+
+# export env variables
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+(
+	(docker -v || echo "Docker not found.") &&
+	(docker-compose up -d || echo "Make sure docker is running.") &&
+	docker exec -ti todo_ls_backend npm test &&
+	docker exec -ti todo_ls_backend npm run migrate &&
+	docker exec -ti todo_ls_backend npm run seed &&
+	echo "App is now available at http://localhost:${FRONTEND_CONTAINER_PORT}"
+) || echo "If you had started the app before, you should shut down the containers, remove the images and run this again."
